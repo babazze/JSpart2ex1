c549694a1f2c2287244529ccaf8d51d1d4fdae98
babazze <babazze@live.fr>
2019-12-02 09:36:55 +0100

    corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var img = document.getElementById('image0');
-//img.addEventListener('Mouseover', changeImage); <- cette ligne ne sert pas car la function backImage est appaler avec l'attribut "onmouseover"
+//img.addEventListener('Mouseover', changeImage); <- cette ligne ne sert pas car la function backImage est appellé avec l'attribut "onmouseover"
 function changeImage()
 {
-  img.src="assets/img/dessin2.png"
+  img.src="assets/img/dessin2.png";
 }
-//img.addEventListener('Mouseout', backImage); <- cette ligne ne sert pas car la function backImage est appaler avec l'attribut "onmouseout"
+//img.addEventListener('Mouseout', backImage); <- cette ligne ne sert pas car la function backImage est appellé avec l'attribut "onmouseout"
 function backImage()
 {
-  img.src="assets/img/dessin1.png"
+  img.src="assets/img/dessin1.png";
 }
